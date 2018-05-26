51114c76af0b01f0bf8e17150b8b00cd3dcee270
Bittech Consulting <bittechconsulting@Bittechs-iMac.home>
2018-05-26 13:27:31 -0400

    add project

---
+++ BDDCucumber_BTC171224M/src/test/java/Runner.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"/Users/bittechconsulting/eclipse-workspace/BDDCucumber_BTC171224M/driven.feature"})
+
+public class Runner {
+
+}
